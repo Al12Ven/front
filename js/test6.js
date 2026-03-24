5862748e6e1ae0d7115920f73d6c3b1709219993
Yakov Sadovkiy and Ivanova Milana <j85984168@gmail.com>
2026-03-24 16:52:14 +0300

    отправка котегорий

---
+++ js/test6.js
@@ -0,0 +1,41 @@
+// Функция отправки запроса по id
+async function id_to_server(id, obj_dom) {
+	let url = `http://localhost/myserver/?id=${id}`
+	let response = await fetch(url, {
+		method: 'GET',
+		headers: { Accept: 'application/json' },
+	})
+
+	let param = await response.json()
+
+	obj_dom.innerHTML = param[0].name
+	obj_dom.classList.add('active')
+
+	//добавление элементов в цикле
+	// for (let key in param) {
+	// 	if (param.hasOwnProperty(key)) {
+	// 		const par = document.querySelector('.block')
+	// 		const newDiv = document.createElement('div')
+	// 		newDiv.className = 'block_inner'
+	// 		output_str = param[key].fam + param[key].name + param[key].ote
+	// 		newDiv.innerHTML = output_str
+
+	// 		par.appendChild(newDiv)
+	// 	}
+	// }
+}
+
+// Получение id по клику и отправка запроса на сервер
+;[...document.querySelectorAll('a')].forEach((item, index) => {
+	item.addEventListener('click', e => {
+		let id = item.dataset.id
+		let block_cont = document.querySelectorAll('.block_cont')
+		for (var i = 0; i < block_cont.length; i++) {
+			if (block_cont[i].classList.contains('active')) {
+				block_cont[i].classList.remove('active')
+			}
+		}
+		//
+		id_to_server(id, block_cont[index])
+	})
+})
